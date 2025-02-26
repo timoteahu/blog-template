a5a26aca0bd958ce9ea53c337523f496a2e9dffb
timoteahu <hutimk@gmail.com>
2025-02-26 18:40:14 -0500

    published analytics

--- my-blog/src/app/layout.tsx
+++ my-blog/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css';
 import { ReactNode } from 'react';
-
+import { Analytics } from "@vercel/analytics/react"
 export const metadata = {
   icon: {
     icon: '/favicon.ico',
@@ -13,6 +13,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body className='bg-white dark:bg-gray-900 transition-colors duration-300'>{children}</body>
+      <Analytics />
     </html>
   );
 }
